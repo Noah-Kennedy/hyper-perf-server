4f1073977a018996fe37edbf4522623d6e203dfc
noah <noah@despise.computer>
2025-01-20 16:52:29 -0600

    wrong port idiot

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ async fn main() {
         .route("/void", post(upload_void));
 
     // run https server
-    let addr = SocketAddr::from(([0, 0, 0, 0], 80));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 443));
 
     tracing::info!("listening on {}", addr);
 
